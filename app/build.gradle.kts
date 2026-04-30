8399646b225c25ea7aacc91d848ee95a12ab8af0
Yusuf Ulgen <ysfulgen142@gmail.com>
2026-05-01 01:45:11 +0300

    sürüm yükseltildi.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.yusufulgen.cuzdan"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.0"
+        versionCode = 4
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
